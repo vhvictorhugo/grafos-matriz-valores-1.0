3ae2a3cc130dbae9820b838a8aa7b9a05f2ccecf
Victor Hugo <victor.h.santos@ufv.br>
2022-02-04 15:24:31 -0300

    criada leitura de arquivo

--- src/grafo.py
+++ src/grafo.py
@@ -13,68 +13,25 @@ class Grafo(object):
 
     def atribuiPeso(self, linha, coluna, peso):
 
-        self.matriz[linha][coluna] = peso
-        self.matriz[coluna][linha] = peso
-
-#     def get_vertices(self):
-#         """ Retorna a lista de vértices do grafo. """
-#         return list(self.adj.keys())
+        self.matriz[linha-1][coluna-1] = peso
+        self.matriz[coluna-1][linha-1] = peso
 
 
-#     def get_arestas(self):
-#         """ Retorna a lista de arestas do grafo. """
-#         return [(k, v) for k in self.adj.keys() for v in self.adj[k]]
+# execucao
+arquivo = open('C:\\Users\\victo\\Desktop\\Grafos-TPI\\src\\grafo.txt', 'r')
 
+n = int (arquivo.readline())
 
-#     def adiciona_arestas(self, arestas):
-#         """ Adiciona arestas ao grafo. """
-#         for u, v in arestas:
-#             self.adiciona_arco(u, v)
+print (n, type(n))
 
+grafo = Grafo(n)
 
-#     def adiciona_arco(self, u, v):
-#         """ Adiciona uma ligação (arco) entre os nodos 'u' e 'v'. """
-#         self.adj[u].add(v)
-#         # Se o grafo é não-direcionado, precisamos adicionar arcos nos dois sentidos.
-#         self.adj[v].add(u)
+grafo.inicializaMatriz(n)
 
+for linha in arquivo:
+    linha = linha.split(' ')
+    grafo.atribuiPeso((int (linha[0])), (int (linha[1])), (float (linha[2].replace('\n', ''))))
 
-#     def existe_aresta(self, u, v):
-#         """ Existe uma aresta entre os vértices 'u' e 'v'? """
-#         return u in self.adj and v in self.adj[u]
-
-
-#     def __len__(self):
-#         return len(self.adj)
-
-
-#     def __str__(self):
-#         return '{}({})'.format(self.__class__.__name__, dict(self.adj))
-
-
-#     def __getitem__(self, v):
-#         return self.adj[v]
-
-
-# # Cria a lista de arestas.
-# arestas = [('A', 'B'), ('B', 'C'), ('B', 'D'), ('C', 'B'), ('C', 'E'), ('D', 'A'), ('E', 'B')]
-
-# # Cria e imprime o grafo.
-# grafo = Grafo(arestas)
-# print(grafo.adj)
-
-# print(grafo.get_vertices())
-
-# print(grafo.get_arestas())
-
-# print(grafo.existe_aresta('A', 'B'), grafo.existe_aresta('E', 'C'))
-
-grafo = Grafo(5)
-
-grafo.inicializaMatriz(5)
-
-print(grafo.matriz)
-
-grafo.atribuiPeso(1, 2, 1.2)
+arquivo.close()
 
 print(grafo.matriz)
